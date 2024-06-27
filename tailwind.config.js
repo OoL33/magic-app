c1579ff99bb6ac8c8d980f6f8b4ccbde520c8308
Alana Lee <alanalee@tutanota.com>
2024-06-27 17:25:31 -0400

    Added display setting for User System Preference

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class',
     content: [
-               "./cmd/web/**/*.html", "./cmd/web/**/*.templ",
+        "./cmd/web/**/*.html", "./cmd/web/**/*.templ",
     ],
     theme: {
         extend: {},
